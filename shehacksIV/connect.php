1a48ddaed7e70dd9e74c96627a973783d2dd986a
Alex Drazilov <alexdrazilov@gmail.com>
2020-01-11 20:15:44 +0000

    updating the connect file

--- shehacksIV/connect.php
+++ shehacksIV/connect.php
@@ -3,14 +3,11 @@
 $dbhost = "db";
 $dbuser= "root";
 $dbpass = "shehacksIV";
-$dbname = "test_db";
+$dbname = "shehacksIV";
 $connection = new mysqli($dbhost, $dbuser,$dbpass,$dbname);
 if (mysqli_connect_errno()) {
  die("Database connection failed :" .
  mysqli_connect_error() . " (" . mysqli_connect_errno() . ")" );
  } //end of if statement
-
-echo 'Succesfully connected';
-
 ?>
 
