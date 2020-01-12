4ad15781b72be51b111a9843be075491a28cf688
Alex Drazilov <alexdrazilov@gmail.com>
2020-01-12 04:22:00 +0000

    Landing page looks good

--- shehacksIV/getcategories.php
+++ shehacksIV/getcategories.php
@@ -4,9 +4,29 @@ include 'connect.php';
 
 $query = "SELECT * FROM categories";
 
-if ($result = $connection->query($queryx)) {
-  echo "Returned rows are: " . $result -> num_rows;
-  // Free result set
-  $result -> free_result();
+if ($result = mysqli_query($connection,$query)) {
+}else{
+	echo 'failed to get categories';
 }
+
+echo '<div class="container">';
+echo '<div class="row">';
+//echo '<div class="col-12">';
+
+while($row = mysqli_fetch_row($result)){
+
+	echo '<div class="col">';
+		echo '<input name="choice" id="';
+		echo $row[0].'" value="" type="checkbox"/>';
+		echo '<label for="';
+		echo $row[0] .'">';
+		echo "<img src='img/category/";
+		echo $row[1]."' alt='".$row[0]."' title='".$row[0]."' style='width: 274px; height: 186px;'>";
+		echo '</label>';
+	echo '</div>';
+}
+
+	echo '</div>';
+echo '</div>';
+
 ?>
--- shehacksIV/index.php
+++ shehacksIV/index.php
@@ -1,7 +1,13 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+      <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
     <title>Home</title>
     <style type="text/css">
 #banner {
@@ -11,6 +17,13 @@ img {
   width: 20vw;
   height: 20vw;
   padding: 2vw;
+}
+
+body{
+
+	background-image: url('img/page-graphics/background.png');
+	background-size: 1940px 1080px;
+
 }
 
 input[type=checkbox] {
@@ -34,122 +47,29 @@ input[type=checkbox]:checked + label > img {
 		include 'connect.php';
 	?>
     <div>
-      <div id="wrapper" style="width:100%; overflow:hidden; "><img style="width: 100%; height: 20%"
+      <div style="width:100%;">
+	  <img style="width: 100%; height: 20%; padding:0;"
           src="img/page-graphics/banner.png"
           alt="banner"
           title="banner">
       </div>
     </div>
-    <div style="text-align: center;"><img src="img/page-graphics/mainpageupdated.png" alt="Invest With Heart" title="main pic"
-    id="banner" style="width: 70%; height: 40%;"></div>
-    <br>
-    <br>
-    <p> </p>
-    <br>
-    <p><br>
-    </p>
-    <p><br>
-    </p>
-    <table style="width: 100%;" border="0">
-      <tbody>
-        <tr>
-          <td style="width: 30.9333px;"><br>
-          </td>
-          <td style="width: 1286.07px;">
-            <p style="font-family: Open Sans; color: #818080; font-size: 40px; text-align: left; line-height: 10px;">Let's
-              begin investing!</p>
-          </td>
-        </tr>
-        <tr>
-          <td><br>
-          </td>
-          <td>
-            <p style="font-family: Open Sans; color: #818080; font-size: 25px; text-align: left; line-height: 10px;">Which
-              causes would you like to help through your investments?</p>
+
+	<br>
+
+            <h1 style="font-family: Open Sans; color: #818080; font-size: 40px; text-align: left; padding-left: 50px;line-height: 10px;">Let's
+              begin investing!</h1>
+		<br>
+            <h2 style="font-family: Open Sans; color: #818080; font-size: 25px; text-align: left; line-height: 10px; padding-left: 50px;">Which
+              causes would you like to help through your investments?</h2>
             <p style="font-family: Open Sans; color: #818080; font-size: 25px; text-align: left; line-height: 10px;"><br>
             </p>
-            <br>
-            <table style="width: 100%;" border="0">
-              <tbody>
-                <tr>
-                  <td style="width: 277px;"><input name="choice" id="choose-1" value="1"
-                      type="checkbox">
-                    <label for="choose-1"> <img src="img/category/womensrights.png"
-                        alt="Women's Rights"
-                        title="Women's Rights"
-                        style="width: 274px; height: 186px;">
-                    </label><br>
-                  </td>
-                  <td style="width: 47.1333px;"><br>
-                  </td>
-                  <td style="width: 277px;"><input type="checkbox" name="choice" id="choose-2" value="2"/>
-                      <label for="choose-2">
-                        <img src="img/category/environment.png" alt="Environment"
-                        title="Environment"
-                        style="width: 274px; height: 186px;">
-                      </label><br>
-                  </td>
-                    
-                  <td style="width: 54.9667px;"><br>
-                  </td>
-                    
-                    <td style="width: 277px;"><input type="checkbox" name="choice" id="choose-3" value="3"/>
-                      <label for="choose-3">
-                        <img src="img/category/animals.png" alt="Animals"
-                        title="Animals"
-                        style="width: 274px; height: 186px;">
-                      </label><br>
-                  </td>
-                  <td style="width: 608.967px;"><br>
-                  </td>
-                </tr>
-                <tr>
-                  <td><br>
-                  </td>
-                  <td><br>
-                  </td>
-                  <td style="height: 47.2px;"><br>
-                  </td>
-                  <td><br>
-                  </td>
-                  <td><br>
-                  </td>
-                </tr>
-                <tr>
-                  <td style="width: 277px;"><input type="checkbox" name="choice" id="choose-4" value="4"/>
-                      <label for="choose-4">
-                        <img src="img/category/communitydevelopment.png" alt="Community Development"
-                        title="Community Development"
-                        style="width: 274px; height: 186px;">
-                      </label><br>
-                  </td>
-                  <td><br>
-                  </td>
-                  <td style="width: 277px;"><input type="checkbox" name="choice" id="choose-5" value="5"/>
-                      <label for="choose-5">
-                        <img src="img/category/health.png" alt="Health"
-                        title="Health"
-                        style="width: 274px; height: 186px;">
-                      </label><br>
-                  </td>
-                  <td><br>
-                  </td>
-                  <td style="width: 277px;"><input type="checkbox" name="choice" id="choose-6" value="6"/>
-                      <label for="choose-6">
-                        <img src="img/category/education.png" alt="Education"
-                        title="Education"
-                        style="width: 274px; height: 186px;">
-                      </label><br>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <br>
+
+	<input type="checkbox" id="myCheckbox1" />
+        <?php include 'getcategories.php'; ?>
+	</input>
+
        <div style="text-align: right;"><img src="img/page-graphics/next.png" alt="Next Page" title="Next Page"
-    style="width: 10%; height: 10%;"></div>
+    style="width: 20%; height: 20%;"></div>
   </body>
 </html>
